90974abb5d3040a0ba0390f887e4d8cd7ec9ae14
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-09-26 14:48:30 +0300

    creating the  main component

---
+++ Weather-app/src/components/WeatherSearch.tsx
@@ -0,0 +1,10 @@
+function WeatherSearch () {
+    return <div className="main-container"> 
+        <input className="searchField" type="text" placeholder=""/>
+        <button className="search-button">
+            Search
+        </button>
+    </div>
+}
+
+export default WeatherSearch;
