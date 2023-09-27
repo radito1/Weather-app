6472098249536db25a99fb5f74f4d4d716a8a386
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-09-27 19:46:31 +0300

    logic for getting the data  from the api

--- Weather-app/src/components/WeatherSearch.tsx
+++ Weather-app/src/components/WeatherSearch.tsx
@@ -1,25 +1,50 @@
 import { ChangeEvent, useState } from "react";
 
 function WeatherSearch() {
-
-  const [input, setInput] = useState('');
-  const [weather,setWeather] = useState();
+  const [input, setInput] = useState("");
+  const [weather, setWeather] = useState();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
-const fetchData = () => {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=2&appid=c6bb3ac90e06b38201400c242a9404c2`)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        setWeather(data);
-    })
+  const fetchWeatherByCoordinates = (lat: string, lon: string) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c6bb3ac90e06b38201400c242a9404c2&units=metric`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((weatherData) => {
+        setWeather(weatherData);
+        console.log("Weather data by coordinates:", weather);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data by coordinates:", error);
+      });
+  };
 
-    console.log(weather);
-}
+  const fetchData = (location: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=c6bb3ac90e06b38201400c242a9404c2`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.length > 0) {
+          const { lat, lon } = data[0];
+          fetchWeatherByCoordinates(lat, lon);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  };
+
+  const handleSearchClick = () => {
+    fetchData(input);
+  };
 
   return (
     <div className="main-container">
@@ -29,7 +54,9 @@ const fetchData = () => {
         value={input}
         onChange={handleChange}
       />
-      <button className="search-button" onClick={fetchData}>Search</button>
+      <button className="search-button" onClick={handleSearchClick}>
+        Search
+      </button>
       <p>Your input: {input}</p>
     </div>
   );
