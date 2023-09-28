adf3d1921ed8aaec146bd1644907a19177e2b7cd
Radoslav Kehadzkiyski <130061726+radito1@users.noreply.github.com>
2023-09-28 11:57:11 +0300

    render icons instead of text

--- Weather-app/src/components/WeatherCard.tsx
+++ Weather-app/src/components/WeatherCard.tsx
@@ -1,14 +1,18 @@
 function WeatherCard(props) {
-    
-    return (
+
+    const iconBaseUrl = "http://openweathermap.org/img/w/";
+
+    const iconUrl = `${iconBaseUrl}${props.data.weather[0].icon}.png`;
+
+    return ( 
         <div className="weather-card-container">
             <div className="top-section">
-                <div>{props.data.weather[0].main}</div>
+                <img src={iconUrl} alt="Weather Icon" />
             </div>
 
             <div className="bottom-section">
                 <div className="current-temp-container">
-                    <div> Current temp: {props.data.main.temp} °C </div>
+                    <div> Current temp: {props.data.main.temp} °C</div>
                 </div>
                 <div className="min-max-temp-container">
                     <div>Min temp: {props.data.main.temp_min} °C</div>
